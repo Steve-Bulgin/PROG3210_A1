a21c447a674fbafa43aaa374dadc90fe041549c4
Steven Bulgin <Sbulgin-cc@conestogac.on.ca>
2016-09-19 15:12:21 -0400

    getting started

---
+++ TempConverter/app/src/main/java/tempconverter/bulgin/com/tempconverter/TempConverterActivity.java
@@ -0,0 +1,19 @@
+/* FileName: TempConverterActivity.java
+ * Purpose: Logic for temp converter
+ * Revision History
+ * 		Steven Bulgin, 	2016.09.19: Created
+ */
+
+package tempconverter.bulgin.com.tempconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TempConverterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp_converter);
+    }
+}
