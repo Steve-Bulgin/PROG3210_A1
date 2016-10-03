d0bc9d4b5bad2a4a969b479e3b3ca93e290f0486
Steven Bulgin <Sbulgin-cc@conestogac.on.ca>
2016-10-03 19:56:36 -0400

    Close

--- TempConverter/app/src/main/java/tempconverter/bulgin/com/tempconverter/TempConverterActivity.java
+++ TempConverter/app/src/main/java/tempconverter/bulgin/com/tempconverter/TempConverterActivity.java
@@ -6,6 +6,7 @@
  *      Steven Bulgin, 2016.09.28: Checks for overflows work
  *      Steven Bulgin, 2016.09.30: Tried OnFocusChange Listener, no go :-(
  *      Steven Bulgin, 2016.10.03: Used try/catch around the Float parse to solve negatives
+ *      Steven Bulgin, 2016.10.03: Closer to right.
  */
 
 /****** Bugs ******/
@@ -19,146 +20,112 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class TempConverterActivity extends AppCompatActivity {
+public class TempConverterActivity extends AppCompatActivity  {
 
     //Variables
     private EditText txtFarenheit;
     private EditText txtCelcius;
     private float farenheit = 0f;
     private float celsius = 0f;
-    private boolean focusChk = false; // t = farenheit, f = celcius;
+    //private boolean focusChk = false; // t = farenheit, f = celcius;
+    private String celsiusstr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp_converter);
         txtFarenheit = (EditText) findViewById(R.id.txtFarenheit);
-        txtFarenheit.setOnFocusChangeListener(FahrenheitchangeChk);
-        txtFarenheit.setOnTouchListener(TouchFarenheit);
+        //txtFarenheit.setOnFocusChangeListener(FahrenheitchangeChk);
+        //txtFarenheit.setOnTouchListener(TouchFarenheit);
         txtCelcius = (EditText) findViewById(R.id.txtCelcius);
-        txtCelcius.setOnFocusChangeListener(CelsiuschangeChk);
-        txtFarenheit.setOnTouchListener(TouchCelsius);
+        //txtCelcius.setOnFocusChangeListener(CelsiuschangeChk);
+        //txtFarenheit.setOnTouchListener(TouchCelsius);
 
-        txtFarenheit.requestFocus();
+        txtFarenheit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int ActionId, KeyEvent event){
+
+                String s = txtFarenheit.getText().toString();
+
+                if (s.length()>0) {
+                    try {
+                        farenheit = Float.parseFloat(s.toString());
+                    }
+                    catch (Exception e) {
+                        Toast.makeText(getApplicationContext(), s.toString(), Toast.LENGTH_SHORT).show();
+                    }
+
+                    celsius = (farenheit - 32) * 0.5556f;
+
+                    //Check overflows
+                    if (celsius < Float.MAX_VALUE && celsius != Float.NEGATIVE_INFINITY) {
+                        txtCelcius.setText(String.format("%.2f", celsius));
+                    }
+                    else if (celsius > Float.MAX_VALUE) {
+                        txtCelcius.setText("Value is too high");
+                    }
+                    else if (celsius == Float.NEGATIVE_INFINITY) {
+                        txtCelcius.setText("Value is too low");
+                    }
+                }
+                else {
+                    txtCelcius.setText("Enter a value");
+                }
+                farenheit = 0f;
+                s = null;
+                return true;
+            }
+        });
+
+        txtCelcius.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int ActionId, KeyEvent event){
+
+                String s = txtCelcius.getText().toString();
+
+                if (s.length() > 0) {
+                    try {
+                        celsius = Float.parseFloat(s.toString());
+                    }
+                    catch (Exception e){
+                        Toast.makeText(getApplicationContext(), s.toString(), Toast.LENGTH_SHORT).show();
+                    }
+
+                    farenheit = (celsius * 1.8f) + 32f;
+
+                    //Check overflows
+                    if (farenheit < Float.MAX_VALUE && farenheit != Float.NEGATIVE_INFINITY) {
+                        txtFarenheit.setText(String.format("%.2f", farenheit));
+                    }
+                    else if (farenheit > Float.MAX_VALUE) {
+                        txtFarenheit.setText("Value is too high");
+                    }
+                    else if (farenheit == Float.NEGATIVE_INFINITY) {
+                        txtFarenheit.setText("Value is too low");
+                    }
+                }
+                else {
+                    txtFarenheit.setText("Enter a value");
+                }
+
+                s = null;
+                celsius = 0f;
+                return false;
+            }
+        });
+
+
+
+        txtCelcius.requestFocus();
     }
-
-    private View.OnTouchListener TouchFarenheit = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            focusChk = true;
-            return false;
-        }
-    };
-
-    private View.OnTouchListener TouchCelsius = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            focusChk = false;
-            return false;
-        }
-    };
-
-    private View.OnFocusChangeListener FahrenheitchangeChk = new View.OnFocusChangeListener() {
-        @Override
-        public void onFocusChange(View v, boolean hasFocus) {
-            if (hasFocus) {
-                txtFarenheit.addTextChangedListener(new TextWatcher() {
-                    @Override
-                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                    }
-
-                    @Override
-                    public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                    }
-
-                    //When input in txtFarenheit changes this is fired to calculate celcius
-                    @Override
-                    public void afterTextChanged(Editable s) {
-                        if (s.length()>0) {
-                            try {
-                                farenheit = Float.parseFloat(s.toString());
-                            }
-                            catch (Exception e) {
-                                Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
-                            }
-
-                            celsius = (farenheit - 32) * 0.5556f;
-
-                            //Check overflows
-                            if (celsius < Float.MAX_VALUE && celsius != Float.NEGATIVE_INFINITY) {
-                                txtCelcius.setText(String.format("%.2f", celsius));
-                            }
-                            else if (celsius > Float.MAX_VALUE) {
-                                txtCelcius.setText("Value is too high");
-                            }
-                            else if (celsius == Float.NEGATIVE_INFINITY) {
-                                txtCelcius.setText("Value is too low");
-                            }
-                        }
-                        else {
-                            txtCelcius.setText("Enter a value");
-                        }
-                    }
-                });
-            }
-        }
-    };
-
-    private View.OnFocusChangeListener CelsiuschangeChk = new View.OnFocusChangeListener() {
-        @Override
-        public void onFocusChange(View v, boolean hasFocus) {
-            if (hasFocus) {
-                txtCelcius.addTextChangedListener(new TextWatcher() {
-                    @Override
-                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                    }
-
-                    @Override
-                    public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                    }
-
-                    @Override
-                    public void afterTextChanged(Editable s) {
-                        if (s.length() > 0) {
-                            try {
-                                celsius = Float.parseFloat(s.toString());
-                            }
-                            catch (Exception e){
-                                Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
-                            }
-
-                            farenheit = (celsius * 1.8f) + 32f;
-
-                            //Check overflows
-                            if (farenheit < Float.MAX_VALUE && farenheit != Float.NEGATIVE_INFINITY) {
-                                txtFarenheit.setText(String.format("%.2f", farenheit));
-                            }
-                            else if (farenheit > Float.MAX_VALUE) {
-                                txtFarenheit.setText("Value is too high");
-                            }
-                            else if (farenheit == Float.NEGATIVE_INFINITY) {
-                                txtFarenheit.setText("Value is too low");
-                            }
-                        }
-                        else {
-                            txtFarenheit.setText("Enter a value");
-                        }
-                    }
-                });
-            }
-        }
-    };
-
 
 
 
